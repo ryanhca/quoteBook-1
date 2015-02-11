9cf85b04e0fd675d13d4d1aac25262d6038c26e6
wgo <whgoller@gmail.com>
2015-02-10 21:29:45 -0700

    add quotes and some buttons

--- JS/controller.js
+++ JS/controller.js
@@ -1,6 +1,20 @@
 var app = angular.module('quoteBook');
 
-app.controller("MainController", function($scope){
+app.controller("MainController", function($scope, mainService){
     $scope.test = "Hello There!"
+    $scope.quotes = mainService.getData();
+    
+    $scope.addQuote = function(){
+        
+    }
+    
+    $scope.removeQuote = function(){
+        
+    }
+    
+    $scope.filterQuotes = function(){
+        
+    }
+    
     
 });
